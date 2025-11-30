0a91734c6998877f612edd16c63a223213bcb1d2
codebybiswa <barikbiswajit152@gmail.com>
2025-11-30 12:39:24 +0530

    Production ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import Home from './component/Home'
-import Navbar from './component/Navbar'
-import AboutSection from './component/AboutSection'
+import React from "react";
+import Home from "./component/Home";
+import Navbar from "./component/Navbar";
+import AboutSection from "./component/AboutSection";
+import Services from "./component/services/Services";
+import Footer from "./component/footer/Footer";
 
 const App = () => {
   return (
-    <div className="overflow-hidden">
-      <Home/>
+    <div className="overflow-hidden bg-[#E9E4DC]">
+      <Home />
       <Navbar />
-       <AboutSection/>
-
+      <AboutSection />
+      <Services />
+      <Footer/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
